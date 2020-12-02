0b0a427f97bf38deba5eb04de21a5b6d5f30c8f3
Berney Alec <beral@sevjnet.ch>
2020-12-02 15:14:07 +0100

    Premiers tests hashmap

--- catch2_tests/hashMapTest.cpp
+++ catch2_tests/hashMapTest.cpp
@@ -3,24 +3,116 @@
 
 using namespace std;
 
-TEST_CASE("various dummy tests", "[hashmap]") {
+// TODO: faire avec plusieurs types?
+// TODO: exceptions levées? -> déjà raison?
+// TODO: trouver un moyen de créer des collisions (si possible)
 
+TEST_CASE("Tests", "[hashmap]")
+{
     HashMapWrapper<int> hashMapWrapper;
 
-    SECTION("Empty hashMap") {
+    SECTION("Empty hashMap")
+    {
         REQUIRE(hashMapWrapper.size() == 0);
     }
 
-    SECTION("insert and contains") {
-        for (int i : {2, 4, 6, 8, 10}) {
-            hashMapWrapper.insert(i);
-        }
+   SECTION("Insert 1 element")
+   {
+       hashMapWrapper.insert(1);
+       REQUIRE(hashMapWrapper.contains(1));
+       REQUIRE(hashMapWrapper.size() == 1);
+   }
 
-        for (int i : {2, 4, 6, 8, 10}) {
-            REQUIRE(hashMapWrapper.contains(i));
-        }
+   SECTION("Insert 1 element already in hashmap")
+   {
+      hashMapWrapper.insert(1);
+      REQUIRE(hashMapWrapper.contains(1));
+      REQUIRE(hashMapWrapper.size() == 1);
+   }
 
-        REQUIRE(hashMapWrapper.size() == 5);
-        REQUIRE(!hashMapWrapper.contains(1));
-    }
+   SECTION("Erase 1 element")
+   {
+       hashMapWrapper.erase(1);
+       REQUIRE(hashMapWrapper.contains(1));
+       REQUIRE(hashMapWrapper.size() == 0);
+   }
+
+   SECTION("Erase non existant element")
+   {
+       REQUIRE(!hashMapWrapper.contains(1));
+       hashMapWrapper.erase(1);
+       REQUIRE(hashMapWrapper.size() == 0);
+   }
+
+   SECTION("Insert positive elements")
+   {
+       for (int i : {2, 4, 6, 8, 10})
+       {
+          hashMapWrapper.insert(i);
+       }
+
+       for (int i : {2, 4, 6, 8, 10})
+       {
+          REQUIRE(hashMapWrapper.contains(i));
+       }
+
+       for (int i : {1, 3, 5, 7, 9})
+       {
+          REQUIRE(!hashMapWrapper.contains(i));
+       }
+
+       REQUIRE(hashMapWrapper.size() == 5);
+   }
+
+
+   SECTION("Erase all positive elements")
+   {
+       for (int i : {2, 4, 6, 8, 10})
+       {
+          hashMapWrapper.erase(i);
+       }
+
+       for (int i : {2, 4, 6, 8, 10})
+       {
+          REQUIRE(!hashMapWrapper.contains(i));
+       }
+
+       REQUIRE(hashMapWrapper.size() == 0);
+   }
+
+   SECTION("Insert negative elements")
+   {
+      for (int i : {-2, -4, -6, -8, -10})
+      {
+         hashMapWrapper.insert(i);
+      }
+
+      for (int i : {-2, -4, -6, -8, -10})
+      {
+         REQUIRE(hashMapWrapper.contains(i));
+      }
+
+      for (int i : {-1, -3, -5, -7, -9})
+      {
+         REQUIRE(!hashMapWrapper.contains(i));
+      }
+
+      REQUIRE(hashMapWrapper.size() == 5);
+   }
+
+   SECTION("Erase all negative elements")
+   {
+      for (int i : {-2, -4, -6, -8, -10})
+      {
+         hashMapWrapper.erase(i);
+      }
+
+      for (int i : {-2, -4, -6, -8, -10})
+      {
+         REQUIRE(!hashMapWrapper.contains(i));
+      }
+
+      REQUIRE(hashMapWrapper.size() == 0);
+   }
+
 }
