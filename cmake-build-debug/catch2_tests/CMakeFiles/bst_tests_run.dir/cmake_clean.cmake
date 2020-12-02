69a302104d99b03f77b0ea28a6b891b419e65db1
MelvynHerzig <34660483+MelvynHerzig@users.noreply.github.com>
2020-12-02 13:50:40 +0100

    Init

---
+++ cmake-build-debug/catch2_tests/CMakeFiles/bst_tests_run.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bst_tests_run.dir/bstTest.cpp.obj"
+  "CMakeFiles/bst_tests_run.dir/catch_main.cpp.obj"
+  "bst_tests_run.exe"
+  "bst_tests_run.exe.manifest"
+  "bst_tests_run.pdb"
+  "libbst_tests_run.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bst_tests_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
